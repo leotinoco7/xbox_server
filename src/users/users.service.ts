1aa3d524a515afc11eab3bb9f9bb181fcf23065e
leotinoco7 <leofptinoco7@gmail.com>
2022-06-20 21:30:11 -0300

    fix(prisma): user model

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,11 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(private readonly prisma: PrismaService) {}
+
+  create(dto: CreateUserDto) {
+    return this.prisma.user.create({ data: dto });
   }
 
   findAll() {
