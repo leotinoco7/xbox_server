41bacaca2071200eab910ce8aa5ff5b6fdde868c
leotinoco7 <leofptinoco7@gmail.com>
2022-06-20 21:54:42 -0300

    chore: initial commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,6 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { UsersModule } from './users/users.module';
-import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [PrismaModule, UsersModule],
